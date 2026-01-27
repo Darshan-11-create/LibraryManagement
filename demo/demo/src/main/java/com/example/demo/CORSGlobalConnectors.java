2ac47b6ecccd770d1252d4154e5a25245f4182a9
Darshan <darshanbommineni@gmail.com>
2026-01-27 23:04:35 +0530

    Add Backend

---
+++ demo/demo/src/main/java/com/example/demo/CORSGlobalConnectors.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class CORSGlobalConnectors {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://127.0.0.1:5501"); // your frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
