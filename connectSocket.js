8e01e61d29134ede00b9babd547005e1d269d6fd
Darshan <darshanbommineni@gmail.com>
2026-01-28 23:16:40 +0530

    Updated

--- connectSocket.js
+++ connectSocket.js
@@ -2,7 +2,7 @@ let stompCustomerClient=null;
 let stompBookClient=null;
 function connectSocket(onmsg){
     // let socket=new SockJS("/topic");
-    const socket=new SockJS('http://localhost:8080/ws')
+    const socket=new SockJS('https://librarymanagementbackend-d5pk.onrender.com//ws')
     stompBookClient=Stomp.over(socket);
      stompBookClient.connect({},()=>{
         stompBookClient.subscribe("/topic/books",(msg)=>{
@@ -12,7 +12,7 @@ function connectSocket(onmsg){
 }
 function connectCustomerSocket(onmsg){
     // let socket=new SockJS("/topic");
-    const socket=new SockJS('http://localhost:8080/ws')
+    const socket=new SockJS('https://librarymanagementbackend-d5pk.onrender.com/ws')
     stompCustomerClient=Stomp.over(socket);
      stompCustomerClient.connect({},()=>{
         stompCustomerClient.subscribe("/topic/customer",(msg)=>{
--- demo/demo/src/main/java/com/example/demo/user/CustomerServices.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.user;
-
-public interface CustomerServices {
-    public boolean EnterDetails(Customer customer);
-}
